27e677b390ca68cff377c48497c848b909f04978
mahmoud <mahmoudalaa25492@gmail.com>
2019-04-09 09:57:35 -0400

    fixing cores backend server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    skip_before_action :verify_authenticity_token
+    before_action :set_headers
+    def set_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+        headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+
     def signup
-        # render :json => { :status => :ok, :message => "Success!", :html => "...insert html..." }
-        render text: "text" , content_type: 'text/plain'
+        render :json => { :status => :ok, :message => "Success!", :html => "...insert html..." }
+    end
+    def login
+        render :json => request.raw_post
     end
 end
