9adc1c893e9a05b3c54b82a5ee3e840df21384d8
root <root@localhost.localdomain>
2019-04-09 19:13:14 +0200

    adding error handling for signup and creating signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,22 @@ class UsersController < ApplicationController
     def signup
         request_body= JSON.parse(request.raw_post)
         user = User.new(:name => request_body["first_name"]+" "+request_body["last_name"], :email => request_body["email"], :password => request_body["password"])
-        user.save
-        render :json => { :status => :ok, :message => "Success!", :html => "...insert html...", :data => request_body }
+        if user.save
+            render :json => { :user_id => user.id }
+        else
+            return nil
+        end
     end
     def login
-        render :json => request.raw_post
+        request_body= JSON.parse(request.raw_post)
+        if !User.find_by_email(request_body["email"]).nil?
+            if User.find_by_email(request_body["email"]).password == request_body["password"]
+                render :json => { :user_id => User.find_by_email(request_body["email"]).id }
+            else
+                return nil
+            end
+        else
+            return nil
+        end
     end
 end
