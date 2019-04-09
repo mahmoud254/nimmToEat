58fbdef952b5eff8f6589c886e15cab31248c12b
mahmoud <mahmoudalaa25492@gmail.com>
2019-04-09 11:26:32 -0400

    building signup feature in backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
     def signup
-        render :json => { :status => :ok, :message => "Success!", :html => "...insert html..." }
+        request_body= JSON.parse(request.raw_post)
+        user = User.new(:name => request_body["first_name"]+" "+request_body["last_name"], :email => request_body["email"], :password => request_body["password"])
+        user.save
+        render :json => { :status => :ok, :message => "Success!", :html => "...insert html...", :data => request_body }
     end
     def login
         render :json => request.raw_post
