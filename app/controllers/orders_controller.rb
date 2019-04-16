6bc5dde320556b789e9bc23c6057f6d9c2413b6d
mahmoud <mahmoudalaa25492@gmail.com>
2019-04-16 03:30:52 -0400

    fixing some bugs in routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,7 @@ class OrdersController < ApplicationController
         if @user_data.present?
         @friends = Friendship.where("user_id = ? AND friend_id = ?",params[:id],@user_data[0]["id"].to_i)
          if @friends.present?
-           
+
             render :json => @user_data[0]
 
          else
@@ -26,16 +26,16 @@ class OrdersController < ApplicationController
 
         request_body= JSON.parse(request.raw_post)
         @group_id = Group.where("name = ? AND creator_id = ?",request_body["group_name"], params[:id]).select("id")
-        
+
         if @group_id.present?
          @member_data = Groupmember.joins(:user).where("group_id = ?",@group_id[0]["id"].to_i).select("member_id ,name")
          if @member_data.present?
-         render:json =>  @member_data
+         render :json =>  @member_data
          else
-            render:json =>{:message =>"faild to retrieve data of member"}
+            render :json =>{:message =>"faild to retrieve data of member"}
          end
         else
-            render:json =>{:message => "wrong group name" }
+            render :json =>{:message => "wrong group name" }
             #return nil
         end
 
@@ -46,29 +46,29 @@ class OrdersController < ApplicationController
         request_body = JSON.parse(request.raw_post)
         new_order = Order.new(:meal => request_body["meal"],:restaurant_name =>request_body["restaurant_name"],
                                :menu_image=>request_body["menu_image"],:status =>request_body["status"],:creator_id=>request_body["creator_id"].to_i)
- 
+
          if new_order.save
-          
+
             @order_members = request_body["ordermembers"]
-           
+
             @order_members.each do |t|
-                
+
                 new_member = Ordermember.new(:order_id => new_order["id"].to_i,:member_id=>t["id"])
                 new_member.save
             end
-            
-                    render:json =>{:message => "done" }
-            
+
+                    render :json =>{:message => "done" }
+
                     # render:json =>{:message => "nonsave members" }
                     #return nil
-             
+
 
        else
-           render:json =>{:message => "nonsave order" }
+           render :json =>{:message => "nonsave order" }
             return nil
-        end                      
+        end
 
-            
+
     end
 
     def show_orders
@@ -109,10 +109,59 @@ class OrdersController < ApplicationController
        
             end
              render :json => @order_details
+    end
 
-            
-       
-       end
 
+    def get_order_details
+      @order = Order.where("id = ?",params[:id])
+      render :json => @order
+    end
+
+
+    def invited_friends
+      request_body = JSON.parse(request.raw_post)
+      @invited_members  = Ordermember.where("order_id = ?",params[:id]).pluck(:member_id)
+      @invited_friends = Friendship.where(user_id: request_body["user_id"]).where(friend_id: @invited_members)
+    end
+
+    def get_invited_friends
+      render :json => invited_friends
+    end
+
+    def get_invited_friends_count
+      render :json => invited_friends.count
+    end
+
+
+    def joined_friends
+      request_body = JSON.parse(request.raw_post)
+      @invited_members  = Ordermember.where("order_id = ?",params[:id]).where(invitation_status:"accepted").pluck(:member_id)
+      @invited_friends = Friendship.where(user_id: request_body["user_id"]).where(friend_id: @invited_members)
+    end
+
+    def get_joined_friends
+      render :json => joined_friends
+    end
+
+    def get_joined_friends_count
+      render :json => joined_friends.count
+    end
+
+    def add_order_item
+      request_body = JSON.parse(request.raw_post)
+      new_order_member_item = Ordermember.new(:order_id => params[:id],:member_id =>request_body["member_id"],
+                            :invitation_status=>"accepted",:item =>request_body["item"],:amount=>request_body["amount"],:price=>request_body["price"],:comment=>request_body["comment"].to_i)
+      if new_order_member_item.save
+        render :json =>{:message => "done" }
+      else
+        render :json =>{:message => "nonsave order_item" }
+        return nil
+      end
+    end
+
+    def get_latest_orders
+      latest_orders = Order.select(:meal, :created_at).last(2)
+      render :json => latest_orders
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post "/users/forgot", to: 'users#forgot'
 
   post "/friends", to:'friends#create'
+  post "/friends/activity" , to: 'friends#get_friends_activity' #takes {user_id:2}
   get  "/friends/:id", to:'friends#list_friends'
   post "/friends/:id/delete", to:'friends#delete_friends'
 
@@ -22,7 +23,24 @@ Rails.application.routes.draw do
 
   post "/orders/show",to:'orders#show_orders'
 
-  
+
+  #################### new commits for order page####################
+  #
+  get "/orders/latest" , to:'orders#get_latest_orders'
+  #get order details
+  get "/orders/:id", to:'orders#get_order_details'
+
+  post "/orders/:id/invitedfriends" ,to:'orders#get_invited_friends' #takes {"user_id":2}
+  post "/orders/:id/invitedfriendscount" ,to:'orders#get_invited_friends_count' #takes {"user_id":2}
+
+
+  post "/orders/:id/joinedfriends" ,to:'orders#get_joined_friends' #takes {"user_id":2}
+  post "/orders/:id/joinedfriendscount" ,to:'orders#get_joined_friends_count' #takes {"user_id":2}
+
+
+  post "/orders/:id/add_item" ,to:'orders#add_order_item' #takes {"user_id":2}
+
+
 
 
 end
